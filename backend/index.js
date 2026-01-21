2e522ecb44e6124977e91e0d08cdd8efab7d43ee
Tushar Dhongade <109501445+Tushardhongade@users.noreply.github.com>
2026-01-21 13:16:24 +0530

    Fix CORS origin URL by removing trailing slash

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://job-portal-zeta-blond.vercel.app/",
+  origin: "https://job-portal-zeta-blond.vercel.app",
   credentials: true,
 };
 
